56861ccff8cac0ba5902e6c732b86600bb8730a2
dekelcohen <dekelcohen@users.noreply.github.com>
2022-09-22 23:18:02 +0300

    Update fewshot.py
    OOM GPU errors on colab --> Dec batch size

--- fewshot.py
+++ fewshot.py
@@ -54,7 +54,7 @@ if args.dataset == "agnews":
     scriptformat = "txt"
     cutoff=0.5
     max_seq_l = 128
-    batch_s = 30
+    batch_s = 20
 elif args.dataset == "dbpedia":
     dataset['train'] = DBpediaProcessor().get_train_examples(f"{args.openprompt_path}/datasets/TextClassification/dbpedia/")
     dataset['test'] = DBpediaProcessor().get_test_examples(f"{args.openprompt_path}/datasets/TextClassification/dbpedia/")
@@ -63,7 +63,7 @@ elif args.dataset == "dbpedia":
     scriptformat = "txt"
     cutoff=0.5
     max_seq_l = 128
-    batch_s = 30
+    batch_s = 20
 elif args.dataset == "yahoo":
     dataset['train'] = YahooAnswersTopicsProcessor().get_train_examples(f"{args.openprompt_path}/datasets/TextClassification/yahoo_answers_topics/")
     dataset['test'] = YahooAnswersTopicsProcessor().get_test_examples(f"{args.openprompt_path}/datasets/TextClassification/yahoo_answers_topics/")
@@ -72,7 +72,7 @@ elif args.dataset == "yahoo":
     scriptformat = "json"
     cutoff=0.5
     max_seq_l = 128
-    batch_s = 30
+    batch_s = 20
 elif args.dataset == "imdb":
     dataset['train'] = ImdbProcessor().get_train_examples(f"{args.openprompt_path}/datasets/TextClassification/imdb/")
     dataset['test'] = ImdbProcessor().get_test_examples(f"{args.openprompt_path}/datasets/TextClassification/imdb/")
